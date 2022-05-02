f75fd5a6d8d0b5a8c2d66f28217eb47430fd509a
cristian12345687 <cristianfabianrivas023@gmail.com>
2022-05-02 14:47:20 -0300

    mensaje

---
+++ productos.php
@@ -0,0 +1,74 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <!--cabecera y menu-->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio Web</title>
+
+    <!--para poder usar css-->
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+</head>
+  <!--contenedor-->
+  <div class="container">
+    <br>
+    <div class="row">
+
+<div class="col-md-3">
+
+<!--Mostrar productos-->
+<div class="card">
+<img class="card-img-top" src="img/imagenproducto.jpg" width="100" alt="">
+    <div class="card-body">
+        <h4 class="card-title">Producto</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+     </div>
+</div>
+<!--Mostrar productos-->
+
+</div>
+
+<div class="col-md-3">
+
+<!--Mostrar productos-->
+<div class="card">
+<img class="card-img-top" src="img/imagenproducto.jpg" width="100" alt="">
+    <div class="card-body">
+        <h4 class="card-title">Producto</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+     </div>
+</div>
+<!--Mostrar productos-->
+
+</div>
+
+<div class="col-md-3">
+
+<!--Mostrar productos-->
+<div class="card">
+<img class="card-img-top" src="img/imagenproducto.jpg" width="100" alt="">
+    <div class="card-body">
+        <h4 class="card-title">Producto</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+     </div>
+</div>
+<!--Mostrar productos-->
+
+</div>
+
+<div class="col-md-3">
+
+<!--Mostrar productos-->
+<div class="card">
+<img class="card-img-top" src="img/imagenproducto.jpg" width="100" alt="">
+    <div class="card-body">
+        <h4 class="card-title">Producto</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+     </div>
+</div>
+<!--Mostrar productos-->
+
+</div>
